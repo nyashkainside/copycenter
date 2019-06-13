6d9a4a42a93f11582d12e56c4514d46039c70618
nyashkainside <47482223+nyashkainside@users.noreply.github.com>
2019-06-13 18:56:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	$('.menuBurger').on('click', function(){
+
+		$('.main_menu').slideToggle(300, function(){
+
+			if ($(this).css('display') === 'none'){
+				$(this).removeAttr('style');
+			}
+		});
+	});
+});
+
